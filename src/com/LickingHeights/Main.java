e47e713a946cb83df4e2a95b5081f526533072a4
Rebecca <Rebecca.powers.2021@student.lhschools.org>
2020-03-13 13:45:53 -0400

    Conditional Implemented

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,30 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int original=2;
+        int input;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        System.out.println("please type a number between 1 and 5" );
+        input=keyboard.nextInt();
+
+        if(input>original) {
+            System.out.println("too high");
+                }
+        else if(input<original){
+            System.out.println("too low");
+        }
+        else{
+            System.out.println("you guessed right!");
+        }
+
+
+
     }
 }
