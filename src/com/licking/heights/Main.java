0446ac1079b093db1318c0a985e7e449174e5f67
rebecca Powers <rebecca.powers.2021@student.lhschools.org>
2020-03-30 20:28:13 -0400

    Random Number Generator Implemented

---
+++ src/com/licking/heights/Main.java
@@ -0,0 +1,9 @@
+package com.licking.heights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println((int) (Math.random()*100+1));
+    }
+}
